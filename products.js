e184974fd0475bdba60e781f6a85517f2f6bd61f
RamonaHTG <47509011+RamonaHTG@users.noreply.github.com>
2019-03-19 11:16:15 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,20 @@
+class Product {
+    constructor (name, type, colour, description, recommendation, price, size) {
+    this.name = name;
+    this.type = type;
+    this.colour = colour; 
+    this.description = description; 
+    this.recommendation = recommendation; 
+    this.price = price;
+    this.size = size;
+    }
+}
+
+var beer1 = new Product("My big fat pale ale", "Pale Ale", "Medium amber", "Light malt flavour, with tropical and floral qualities. Dry and bitter aftertaste.", "Try this if you enjoy rom-coms and stand-up comedies.", "50 DKK.", "330 ml."); 
+var beer2 = new Product("Pretty Fly for the Rye", "Rye Beer", "Red Brown ", "Round spiciness from the rye, with a balanced malty sweetness.", "Try this if you are still listen to 90s music.", "50 DKK.", "330 ml."); 
+var beer3 = new Product("Fsociety", "Brown Ale", "Dark brown", " Plum and raisins flavour, veering towards a serious nutty style.", "Try this if you are secretly ploting to take down E Corp.", "50 DKK.", "330 ml."); 
+var beer4 = new Product("Mother of Porters", "Porter", "Mahogany", "Light malt sweetness, dark fruit flavours with strong hits of coffee", "Try this if you are on your quest to sit on the Iron Throne", "50 DKK.", "330 ml."); 
+var beer5 = new Product("Legally Blonde", "Cider", "Rose", "Crisp and refreshing. Funky raspberries, pink apples and strawberries", "Try this if you are sassy and you know it", "50 DKK.", "330 ml."); 
+var beer6 = new Product("Danish Pie", "Pilsner", "Light", "Bitter qualities, with notes of lemongrass.", "Try this if you went one time to a band camp", "50 DKK.", "330 ml."); 
+
+console.log(Product);
