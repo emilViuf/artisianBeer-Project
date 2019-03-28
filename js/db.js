f8fab25c40c904db77e8ef9aabc6fba24a1bdc33
Ramona HTG <ramona.a.hategan@gmail.com>
2019-03-28 02:39:38 +0100

    changes

--- js/db.js
+++ js/db.js
@@ -3,7 +3,7 @@ var products = [];
 var beer1 = new Product("My big fat pale ale", 01, "Pale Ale", "Medium amber", "Light malt flavour, with tropical and floral qualities. Dry and bitter aftertaste.", "50 DKK.", "330 ml."); 
 var beer2 = new Product("Pretty Fly for the Rye", 02, "Rye Beer", "Red Brown ", "Round spiciness from the rye, with a balanced malty sweetness.", "50 DKK.", "330 ml."); 
 var beer3 = new Product("Fsociety", 03, "Brown Ale", "Dark brown", " Plum and raisins flavour, veering towards a serious nutty style.", "50 DKK.", "330 ml."); 
-var beer4 = new Product("Game of Porters", 04, "Porter", "Mahogany", "Light malt sweetness, dark fruit flavours with strong hits of coffee",  "50 DKK.", "330 ml."); 
+var beer4 = new Product("Game of Porters", 04, "Porter", "Mahogany", "Light malt sweetness, dark fruit flavours with strong hits of coffee",  "50 DKK.", "330 ml.", "images/porter.jpg"); 
 var beer5 = new Product("Legally Blonde", 05, "Cider", "Rose", "Crisp and refreshing. Funky raspberries, pink apples and strawberries", "50 DKK.", "330 ml."); 
 var beer6 = new Product("Danish Pie", 06, "Pilsner", "Light", "Bitter qualities, with notes of lemongrass.", "50 DKK.", "330 ml."); 
 
--- js/product.js
+++ js/product.js
@@ -1,24 +1,44 @@
 class Product {
-    constructor (name, id,  type, colour, description, price, size) {
-    this.name = name;
-    this.id = id; 
-    this.type = type;
-    this.colour = colour; 
-    this.description = description; 
-    this.price = price;
-    this.size = size;
-    }
+    
+    constructor (name, id,  type, colour, description, price, size,img) {
+        this.name = name;
+        this.id = id; 
+        this.type = type;
+        this.colour = colour; 
+        this.description = description; 
+        this.price = price;
+        this.size = size;
+        this.img = img;
+        }
 
 productInformation () { 
-    return '<tr><td>' + this.name + '</td>' + 
-           '<td>' + this.id + '</td>' + 
-           '<td>' + this.type + '</td>' + 
-           '<td>' + this.colour + '</td>' + 
-           '<td>' + this.description + '</td>' + 
-           '<td>' + this.price + '</td>' +  
-           '<td>' + this.size + '</td></tr>'; 
+    return '<table border><tr>'+
+    '<td colspan="2"><img src="'+this.img+'" alt="Smiley face" height="242" width="142"></td></tr><tr>' +
+            '<td>' + this.name + '</td>' + 
+           '<td>' + this.size + '</td></tr><tr>' + 
+          
+           '<td>' + this.type + '</td>' +  
+           '<td>' + this.price + '</td></tr></table>'; 
     }
+
+
+
+    productInformationDetail () { 
+        return '<table ><tr><td>' + this.name + '</td>' + 
+               '<td>' + this.id + '</td>' + 
+               '<td>' + this.type + '</td>' + 
+               '<td>' + this.colour + '</td></tr><tr>' + 
+               '<td>' + this.description + '</td></tr><tr>  ' + 
+               '<td>' + this.price + '</td>' +  
+               '<td>' + this.size + '</td></tr></table>'; 
+        }
+
+
+
+
 }
+
+
 
 console.log(Product.productInformation);
  
