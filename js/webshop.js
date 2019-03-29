e1b0f52e210bd5063ba64875d12be90bd8ac3338
Ramona HTG <ramona.a.hategan@gmail.com>
2019-03-29 09:14:28 +0100

    new commit

--- js/webshop.js
+++ js/webshop.js
@@ -1,26 +1,9 @@
 
 var htmlTableVariable = '<table border>';
-//htmlTableVariable +='<tr>';
-htmlTableVariable +='<tr>';   
-    var count = 0;
-        for (let i = 0; i < products.length/2; i++) {
-      
-            htmlTableVariable +='<td>';
-            htmlTableVariable += products[i].productInformation();
-            htmlTableVariable +='</td>';
-            
-        }
-        htmlTableVariable +='</tr>';
-        htmlTableVariable +='<tr>';   
-        var count = 0;
-            for (let i = products.length/2; i < products.length; i++) {
-              
-                htmlTableVariable +='<td>';
-                htmlTableVariable += products[i].productInformation();
-                htmlTableVariable +='</td>';
-               
-            }
-            htmlTableVariable +='</tr>';
-htmlTableVariable+= '</table>';
+
+        for (let i = 0; i < products.length; i++) {
+    htmlTableVariable+=products[i].productInformation()
+}
+htmlTableVariable+='</table>';
 document.getElementById('showProducts').innerHTML = htmlTableVariable;
 
